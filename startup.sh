5633ccc5f14a98a2668425def18574d126503930
Hider-alt <alessio5.magnani@gmail.com>
2023-03-05 15:42:03 +0100

    Edit startup file

--- startup.sh
+++ startup.sh
@@ -2,13 +2,19 @@
 
 # Load .env file
 set -o allexport
-source .env
-set +o allexport
 
 git config --global user.password $GIT_PASSWORD
 git clone https://github.com/Hider-alt/ITIBot
-cd ITIBot
+
+# Extract ITIBot folder content to root
+cp -r ITIBot/* .
+rm -rf ITIBot
+
 git fetch
 git pull
+
+# Create virtual environment
+python -m venv venv
+
 pip install -r requirements.txt
 python main.py

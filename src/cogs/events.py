b11f4ffd1bb4c68edd472edefb69fa155bd14ed7
hider <alessio5.magnani@gmail.com>
2025-02-09 01:52:29 +0100

    Remove check to test

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -22,9 +22,10 @@ class Events(Cog):
     async def check_variations_loop(self):
         now = datetime.now(pytz.timezone('Europe/Rome'))
 
-        if 0 < now.hour < 6:
-            print(f"[{now}] Skipping Variations Check (0 < now hour < 6)")
-            return
+        # TODO
+        #if 0 < now.hour < 6:
+        #    print(f"[{now}] Skipping Variations Check (0 < now hour < 6)")
+        #    return
 
         print(f"[{now}] Checking Variations")
         await refresh_variations(self.bot)
